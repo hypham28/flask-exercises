ff708d5d11994d5b97c24b3c99542962d537f6a3
hypham28 <hpham28@uncc.edu>
2023-10-05 18:41:49 -0400

    adding css template

--- flask1/app.py
+++ flask1/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def current_time():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return render_template("index.html", current_time=current_time)
 
 if __name__ == "__main__":
--- flask2/app.py
+++ flask2/app.py
@@ -2,30 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-def check_number(number):
-    try:
-        num = int(number)
-        if num % 2 == 0:
-            return f"{num} is an even number."
-        else:
-            return f"{num} is an odd number."
-    except ValueError:
-        return "Not a valid integer."
-
 @app.route("/", methods=["GET", "POST"])
-def index():
+def home():
     if request.method == "POST":
-        user_input = request.form.get("number")
-        return redirect(url_for("result", number=user_input))
+        number = request.form.get("number")
+        if number.isdigit():
+            number = int(number)
+            return redirect(url_for("result", number=number))
     return render_template("index.html")
 
-@app.route("/result")
-def result():
-    user_input = request.args.get("number")
-    if user_input is None:
-        return render_template("result.html", error="No query parameter provided.")
-    message = check_number(user_input)
-    return render_template("result.html", user_input=user_input, message=message)
+@app.route("/result/<int:number>")
+def result(number):
+    is_even = "Even" if number % 2 == 0 else "Odd"
+    return render_template("result.html", number=number, is_even=is_even)
 
 if __name__ == "__main__":
     app.run(debug=True)
